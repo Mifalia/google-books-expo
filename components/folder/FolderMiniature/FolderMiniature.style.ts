7d91148777a1682ab8559ed68424baa980fa90ca
mifalia <mifarivonjisoa@gmail.com>
2025-04-03 11:59:25 +0300

    chore : moved FolderMiniature to dedicated directory

---
+++ components/folder/FolderMiniature/FolderMiniature.style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  miniatureContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderColor: "#e8ecef",
+    gap: 16,
+  },
+
+  iconContainer: {},
+
+  texContainer: {
+    gap: 4,
+  },
+
+  folderTitle: {
+    fontSize: 14,
+    color: "#3F3D56",
+    fontWeight: "bold",
+  },
+
+  folderSmall: {
+    fontSize: 12,
+    color: "#6b7280",
+  },
+});
